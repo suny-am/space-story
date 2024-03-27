9b344643980bdf684cacc2616bb22723dc07bc67
suny-am <visualarea.1@gmail.com>
2024-03-27 12:51:28 +0100

    Basic Vue setup

---
+++ Client/src/components/app-footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer height="40" app>
+    <a
+      v-for="item in items"
+      :key="item.title"
+      :href="item.href"
+      :title="item.title"
+      class="d-inline-block mx-2 social-link"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <v-icon
+        :icon="item.icon"
+        :size="item.icon === '$vuetify' ? 24 : 16"
+      />
+    </a>
+
+    <div
+      class="text-caption text-disabled"
+      style="position: absolute; right: 16px;"
+    >
+      &copy; 2024-{{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">© Carl Sandberg Design Works</span>
+    </div>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+  const items = []
+</script>
+
+<style lang="sass" scoped >
+  .social-link :deep(.v-icon)
+    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
+    text-decoration: none
+    transition: .2s ease-in-out
+
+    &:hover
+      color: rgba(25, 118, 210, 1)
+</style>
---
+++ Client/src/components/main-menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="fill-height" id="main-menu">
+    <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+      <div class="text-center">
+        <div class="text-body-2 font-weight-light mb-n1">visual novel</div>
+
+        <h1 class="text-h2 font-weight-bold">space story</h1>
+      </div>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Aldrich&family=Anta&family=Audiowide&family=Bruno+Ace&family=Bruno+Ace+SC&family=Electrolize&family=Gemunu+Libre:wght@200..800&family=Goldman:wght@400;700&family=Orbitron:wght@400..900&family=Rationale&family=Sarpanch:wght@400;500;600;700;800;900&family=Wallpoet&family=Zen+Dots&display=swap");
+
+body {
+  user-select: none;
+}
+
+.text-center {
+  * {
+    font-family: "Bruno Ace SC";
+  }
+}
+</style>
